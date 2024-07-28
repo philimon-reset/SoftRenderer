94480f514179ee3f2ed72ba1356b795177e2c265
Philimon Derib Ayalew <philimonderib2019@gmail.com>
2024-07-28 18:11:32 +0200

    Add project files.

---
+++ vector.cs
@@ -0,0 +1,36 @@
+﻿namespace Vector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    /// <summary>
+    /// A vector representation.
+    /// </summary>
+    /// <remarks>
+    /// Based on this tutorial : https://www.codeproject.com/articles/17425/a-vector-type-for-c.
+    /// </remarks>
+    public class Vector
+    {
+        private readonly double x;
+        private readonly double y;
+        private readonly double z;
+
+        /// <summary>
+        /// Constructor for the vector
+        /// </summary>
+        /// <param name="x"> x corrdinate of vector</param>
+        /// <param name="y"> y corrdinate of vector</param>
+        /// <param name="z"> z corrdinate of vector</param>
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+    }
+
+
+}
